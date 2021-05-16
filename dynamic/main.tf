6f633d76a255edd043011f4ae0e3d531a3a4d0a7
hhiroki <gyouzakuuka@gmail.com>
2021-05-16 22:12:09 +0900

    update

---
+++ dynamic/main.tf
@@ -0,0 +1,40 @@
+variable node_affinities {
+  type = list(object({
+    key   = string,
+    value = string
+  }))
+  default = [{
+    key   = "env",
+    value = "dev"
+    }, {
+    key   = "layer",
+    value = "front"
+  }]
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test-instance"
+  machine_type = "f1-micro"
+  zone         = "asia-northeast1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  scheduling {
+    dynamic "node_affinities" {
+      for_each = var.node_affinities
+      content {
+        key      = node_affinities.value["key"]
+        operator = "IN"
+        values   = [node_affinities.value["value"]]
+      }
+    }
+  }
+}
