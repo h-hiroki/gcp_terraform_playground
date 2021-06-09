aa249c76050d86cd120116a8a4b9d2d814bf60fc
hhiroki <gyouzakuuka@gmail.com>
2021-06-09 23:02:09 +0900

    Update main.tf

--- two-layer-architecture/main.tf
+++ two-layer-architecture/main.tf
@@ -22,5 +22,16 @@ resource "google_compute_region_instance_group_manager" "default" {
   }
 
   base_instance_name = "mig"
-  target_size = 3
+  target_size = 2
+}
+
+# オートスケール設定
+resource "google_compute_region_autoscaler" "default" {
+  name = "default"
+  target = google_compute_region_instance_group_manager.default.self_link # instance_group_managerに対して設定する
+
+  autoscaling_policy {
+    max_replicas = 5
+    min_replicas = 2
+  }
 }
