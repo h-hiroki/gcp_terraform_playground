cf236022905ac9c6a72f5f4815f6cf8c73121a91
hhiroki <gyouzakuuka@gmail.com>
2021-06-10 14:38:00 +0900

    Update main.tf

--- two-layer-architecture/main.tf
+++ two-layer-architecture/main.tf
@@ -23,6 +23,16 @@ resource "google_compute_region_instance_group_manager" "default" {
 
   base_instance_name = "mig"
   target_size = 2
+
+  auto_healing_policies {
+    health_check = google_compute_health_check.mig_health_check.self_link
+    initial_delay_sec = 30
+  }
+
+  timeouts {
+    // 変更後に10から15分程度かかる時もあるので設定している
+    create = "15m"
+  }
 }
 
 # オートスケール設定
@@ -34,4 +44,39 @@ resource "google_compute_region_autoscaler" "default" {
     max_replicas = 5
     min_replicas = 2
   }
+}
+
+# ヘルスチェックリクエスト。80番ポート利用
+resource "google_compute_health_check" "mig_health_check" {
+  name = "default"
+
+  http_health_check {
+    port = 80
+  }
+}
+
+# ヘルスチェック用の80番ポート利用許可
+resource "google_compute_firewall" "mig_health_check" {
+  name = "health-check"
+  network = "default"
+  
+  allow {
+    protocol = "tcp"
+    ports = [80]
+  }
+
+  source_ranges = [ "130.211.0.0/22", "35.191.0.0./16" ]
+}
+
+# 22ポート利用許可
+resource "google_compute_firewall" "default_ssh" {
+  name = "default-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = [22]
+  }
+  source_ranges = [ "0.0.0.0/0" ]
+  target_tags = [ "allow-ssh" ]
 }
